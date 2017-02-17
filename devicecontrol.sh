7dd4e8849d36a83a037a8cbc0b293993d541d48c
George Tabacar <g.tabacar@purplesrl.com>
2017-02-17 17:31:58 +0200

    fix crontab

--- devicecontrol.sh
+++ devicecontrol.sh
@@ -1,42 +1,43 @@
 #!/bin/bash
 
+PATH=/bin:/sbin
 KERNEL=$(uname -r)
 
 function unmount_usb_drives {
 	USB_DRIVES="$(for devlink in /dev/disk/by-id/usb*; do readlink -f ${devlink}; done)"
 	for usb_drive in $USB_DRIVES; do
 		echo Unmounting $usb_drive
-		/bin/umount -f $usb_drive
+		umount -f $usb_drive
 	done
 }
 function disable_usb {
 	echo "Disabling USB storage"
-	DISABLE="/bin/mv /lib/modules/$KERNEL/kernel/drivers/usb/storage/usb-storage.ko /lib/modules/$KERNEL/kernel/drivers/usb/storage/usb-storage.backup"
-	/bin/unmount_usb_drives
-	/sbin/rmmod uas
-	/sbin/rmmod usb_storage
+	DISABLE="mv /lib/modules/$KERNEL/kernel/drivers/usb/storage/usb-storage.ko /lib/modules/$KERNEL/kernel/drivers/usb/storage/usb-storage.backup"
+	unmount_usb_drives
+	rmmod uas
+	rmmod usb_storage
 	`$DISABLE`
 }
 function enable_usb {
 	echo "Enabling USB storage"
-	ENABLE="/bin/mv /lib/modules/$KERNEL/kernel/drivers/usb/storage/usb-storage.backup /lib/modules/$KERNEL/kernel/drivers/usb/storage/usb-storage.ko"
+	ENABLE="mv /lib/modules/$KERNEL/kernel/drivers/usb/storage/usb-storage.backup /lib/modules/$KERNEL/kernel/drivers/usb/storage/usb-storage.ko"
 	`$ENABLE`
-	/sbin/modprobe usb_storage
+	modprobe usb_storage
 }
 
 function disable_cdrom {
 	echo "Disabling CDROM"
-	/bin/umount -f /dev/sr0
-	DISABLE="/bin/mv /lib/modules/$KERNEL/kernel/drivers/scsi/sr_mod.ko /lib/modules/$KERNEL/kernel/drivers/scsi/sr_mod.backup"
+	umount -f /dev/sr0
+	DISABLE="mv /lib/modules/$KERNEL/kernel/drivers/scsi/sr_mod.ko /lib/modules/$KERNEL/kernel/drivers/scsi/sr_mod.backup"
 	`$DISABLE`
-	/sbin/rmmod sr_mod
+	rmmod sr_mod
 }
 
 function enable_cdrom {
 	echo "Enabling CDROM"
-	ENABLE="/bin/mv /lib/modules/$KERNEL/kernel/drivers/scsi/sr_mod.backup /lib/modules/$KERNEL/kernel/drivers/scsi/sr_mod.ko"
+	ENABLE="mv /lib/modules/$KERNEL/kernel/drivers/scsi/sr_mod.backup /lib/modules/$KERNEL/kernel/drivers/scsi/sr_mod.ko"
 	`$ENABLE`
-	/sbin/modprobe sr_mod
+	modprobe sr_mod
 }
 
 
